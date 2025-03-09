1ede173c2d8d34c0e77736b0eeea924bca4fc50c
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-03-08 18:24:10 -0600

    Locations route

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,14 @@
-export class CreateLocationDto {}
+import { IsArray, IsString, ArrayNotEmpty, MaxLength } from 'class-validator';
+import { Location } from '../entities/location.entity';
+export class CreateLocationDto extends Location {
+    @IsString()
+    @MaxLength(35)
+    locationName: string;
+    @IsString()
+    @MaxLength(120)
+    locationAddress: string;
+    @IsArray()
+    @ArrayNotEmpty()
+    locationLatLng: number[];
+
+}
--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationsController } from './locations.controller';
+import { Location } from './entities/location.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Location])],
     controllers: [LocationsController],
     providers: [LocationsService],
 })
